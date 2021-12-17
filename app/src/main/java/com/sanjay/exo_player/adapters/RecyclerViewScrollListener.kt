d413cbf68c30536fcd56a05186a8a28897e198fc
sanjaydraws <sprajapat8331@gmail.com>
2021-12-17 23:57:03 +0530

    onScrollStateListener

--- app/src/main/java/com/sanjay/exo_player/adapters/RecyclerViewScrollListener.kt
+++ app/src/main/java/com/sanjay/exo_player/adapters/RecyclerViewScrollListener.kt
@@ -1,5 +1,6 @@
 package com.sanjay.exo_player.adapters
 
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -7,6 +8,9 @@ abstract class RecyclerViewScrollListener : RecyclerView.OnScrollListener() {
     private var firstVisibleItem = 0
     private var visibleItemCount = 0
 
+    companion object{
+        const val TAG = "RecyclerScrollListener"
+    }
 
     @Volatile
     private var mEnabled = true
@@ -18,6 +22,8 @@ abstract class RecyclerViewScrollListener : RecyclerView.OnScrollListener() {
             require(manager is LinearLayoutManager) { "Expected recyclerview to have linear layout manager" }
             val mLayoutManager = manager
             visibleItemCount = mLayoutManager.childCount
+            Log.d(TAG, "onScrolled: $visibleItemCount")
+
             firstVisibleItem = mLayoutManager.findFirstCompletelyVisibleItemPosition()
             onItemIsFirstVisibleItem(firstVisibleItem)
         }
@@ -39,5 +45,21 @@ abstract class RecyclerViewScrollListener : RecyclerView.OnScrollListener() {
 
     fun setPreLoadCount(mPreLoadCount: Int) {
         this.mPreLoadCount = mPreLoadCount
+    }
+
+    override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+        super.onScrollStateChanged(recyclerView, newState)
+        when(newState){
+            RecyclerView.SCROLL_STATE_IDLE ->{
+                Log.d(TAG, "onScrollStateChanged: SCROLL_STATE_IDLE")
+            }
+            RecyclerView.SCROLL_STATE_DRAGGING ->{
+                Log.d(TAG, "onScrollStateChanged: SCROLL_STATE_DRAGGING")
+            }
+            RecyclerView.SCROLL_STATE_SETTLING ->{
+                Log.d(TAG, "onScrollStateChanged: SCROLL_STATE_SETTLING")
+
+            }
+        }
     }
 }
